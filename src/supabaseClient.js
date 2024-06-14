110d5c27cf7b952be481cebafc85d20bba4a2583
dhaanuofficial <dhaanu.i@questlabs.biz>
2024-06-14 17:13:39 +0530

    add warning for supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,4 +4,10 @@ import { mainConfig } from "./assets/Config/appConfig";
 const supabaseUrl = mainConfig.REACT_APP_SUPABASE_URL;
 const supabaseKey = mainConfig.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export let supabase = null;
+
+if (supabaseUrl && supabaseKey) {
+  supabase = createClient(supabaseUrl, supabaseKey);
+} else {
+  console.warn("Supabase URL and ANON KEY are not provided. Hense, supabase operations will be skipped.");
+}
