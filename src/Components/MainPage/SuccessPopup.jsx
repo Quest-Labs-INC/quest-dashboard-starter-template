43abf149ffa541d02be849ff9963c1773ee31e48
soumitra-quest <soumitra.d@questlabs.biz>
2024-06-19 20:07:44 +0530

    update repo link

--- src/Components/MainPage/SuccessPopup.jsx
+++ src/Components/MainPage/SuccessPopup.jsx
@@ -86,7 +86,7 @@ const SuccessPopup = ({ setSuccessPopup }) => {
                         <button className='border w-full border-[#6200EE] rounded-[10px] text-zinc-800 text-sm font-semibold font-["Figtree"] px-4 py-2.5' onClick={() => { window.location.href = "/your-app-login" }}>
                             Preview
                         </button>
-                        <button className='text-white w-full text-sm font-semibold font-["Figtree"] px-4 py-2.5 bg-[linear-gradient(84deg,_#9035FF_0.36%,_#0065FF_100.36%)] rounded-[10px]' onClick={() => window.open("https://github.com/Quest-Labs-INC/quest-dashboard-starter-template")}>
+                        <button className='text-white w-full text-sm font-semibold font-["Figtree"] px-4 py-2.5 bg-[linear-gradient(84deg,_#9035FF_0.36%,_#0065FF_100.36%)] rounded-[10px]' onClick={() => window.open("https://github.com/Quest-Labs-INC/quest-dashboard-starter-template/tree/saas-gpt")}>
                             Fork Repo
                         </button>
                     </div>
