ffc35a91b7b2a649778532735c577f2e6dec66d9
soumitra-quest <soumitra.d@questlabs.biz>
2024-06-21 19:26:49 +0530

    add origin link

--- src/Components/Settings/AddAdminPopup.jsx
+++ src/Components/Settings/AddAdminPopup.jsx
@@ -27,6 +27,7 @@ const AddAdminPopup = ({ setAdminPopup, setFlag }) => {
       email: email.toLowerCase(),
       entityId,
       name,
+      inviteLink: window.location.origin,
     };
 
     let request = generalFunction.createUrl(`api/entities/${ownerDetails?.ownerEntityId}/invite`);
