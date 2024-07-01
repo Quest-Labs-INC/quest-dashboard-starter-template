9718eb00aeefddc19c78ac62aae95d2b201470e6
Shraddha  Agrawal <shraddhaagrawal@Shraddhas-MacBook-Pro.local>
2024-07-01 14:55:43 -0700

    test code for local and prod

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 import { mainConfig } from "./assets/Config/appConfig";
 
+const isLocal = window.location.hostname === 'localhost';
+
+if (isLocal) {
+    console.log('Running locally');
+} else {
+    console.log('Running on the cloud');
+}
+
 const supabaseUrl = mainConfig.REACT_APP_SUPABASE_URL;
 const supabaseKey = mainConfig.REACT_APP_SUPABASE_ANON_KEY;
 
